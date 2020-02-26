325b399c35a798a7bf8993f8ec1ee173421f5f00
Javier Aviles <avileslopez.javier@gmail.com>
2020-02-26 14:51:59 +0100

    Removing some unnecessary fields + updating requirements

--- test/0.1.0/test.js
+++ test/0.1.0/test.js
@@ -14,7 +14,6 @@ describe('Har2Postman', function () {
     var postmanCollection = JSON.parse(postmanContent);
     expect(postmanCollection.info.name).toEqual('Har2Postman');
     expect(postmanCollection.info.schema).toEqual('https://schema.getpostman.com/json/collection/v2.1.0/collection.json');
-    expect(postmanCollection.info._postman_id).toMatch('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$');
   });
 
   it('Generates an item in the output with a name composed out of method + url pathname', function () {
@@ -23,11 +22,10 @@ describe('Har2Postman', function () {
     expect(postmanCollection.item[0].name).toEqual('GET /posts/1');
   });
 
-  it('Generates an item in the output with a request containing GET method, no headers and a valid url object', function () {
+  it('Generates an item in the output with a request containing GET method and a valid url object', function () {
     var postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFile));
     var postmanCollection = JSON.parse(postmanContent);
     expect(postmanCollection.item[0].request.method).toEqual('GET');
-    expect(postmanCollection.item[0].request.header).toEqual([]);
     expect(postmanCollection.item[0].request.url).toEqual({ raw: "https://jsonplaceholder.typicode.com/posts/1", protocol: "https", host: ["jsonplaceholder", "typicode", "com"], path: ["posts", "1"] });
   });
 
