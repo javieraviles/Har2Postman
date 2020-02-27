a243da46bde5256c6be58b0ed1cc54a7bde970c3
rpaezbas <rafaelpaezbastida@gmail.com>
2020-02-27 01:14:54 +0100

    small 0.2.0 test refactor

--- test/0.2.0/test.js
+++ test/0.2.0/test.js
@@ -4,12 +4,17 @@ describe('Har2Postman', function () {
     var harFile = JSON.parse(fs.readFileSync(__dirname + '/input.json', 'utf8'));
     var expectedPostmanCollection = JSON.parse(fs.readFileSync(__dirname + '/output.json', 'utf8'));
 
-    it('should include basic test assertion', function() {
+    it('should include item event', function() {
+        var postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFile), true);
+        var postmanCollection = JSON.parse(postmanContent);
+        expect(postmanCollection.item.length).toEqual(1);
+        expect(postmanCollection.item[0].event.length).toEqual(1);
+    });
+
+    it('should generate postman script that asserts call with response -200-', function() {
         var postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFile), true);
         var postmanCollection = JSON.parse(postmanContent);
         var itemEvent = postmanCollection.item[0].event[0];
-        expect(postmanCollection.item.length).toEqual(1);
-        expect(postmanCollection.item[0].event.length).toEqual(1);
         expect(itemEvent.listen).toEqual("test");
         expect(itemEvent.script.type).toEqual("text/javascript");
         expect(itemEvent.script).toBeDefined();
