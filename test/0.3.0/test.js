d018c6d52c74a1eb25b5e65509a36f0c034e779f
Javier Aviles <avileslopez.javier@gmail.com>
2020-02-28 23:54:42 +0100

    Adding query params if present

---
+++ test/0.3.0/test.js
@@ -0,0 +1,12 @@
+describe('Har2Postman', () => {
+  const harToPostman = require('../../lib/har-to-postman.js');
+  const fs = require('fs');
+  const harFile = JSON.parse(fs.readFileSync(__dirname + '/input.json', 'utf8'));
+
+  it('should include query within the url in case query params are present', () => {
+    const postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFile), true);
+    const postmanCollection = JSON.parse(postmanContent);
+    expect(Object.keys(postmanCollection.item[0].request.url)).toContain('query');
+  });
+});
+
