e1f6295e2d395283689886de3b26adf750da06b2
rpaezbas <rafaelpaezbastida@gmail.com>
2020-03-01 16:19:11 +0100

    format fix

--- test/0.4.0/test.js
+++ test/0.4.0/test.js
@@ -6,9 +6,9 @@ const postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFi
 const postmanCollection = JSON.parse(postmanContent);
 
 it('Generates multiple items for the same collection', () => {
-    expect(postmanCollection.item.length).not.toBeLessThan(2);
+  expect(postmanCollection.item.length).not.toBeLessThan(2);
 });
 
 it('Generates 0.4.0 output json (postman) given the 0.4.0 input json (har)', () => {
-    expect(expectedPostmanCollection).toEqual(postmanCollection);
+  expect(expectedPostmanCollection).toEqual(postmanCollection);
 });
