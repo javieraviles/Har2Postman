b7a7b1467f8fa456d255b2c51579b2a31ac02e35
Rafael Paez <rpaezbas@everis.com>
2020-03-10 02:23:42 +0100

    added test for response codes 201,202,204 and 403

---
+++ test/0.6.0/test.js
@@ -0,0 +1,12 @@
+const harToPostman = require('../../lib/har-to-postman.js');
+const fs = require('fs');
+const harFile = JSON.parse(fs.readFileSync(__dirname + '/input.json', 'utf8'));
+const expectedPostmanCollection = JSON.parse(fs.readFileSync(__dirname + '/output.json', 'utf8'));
+const postmanContent = harToPostman.createPostmanCollection(JSON.stringify(harFile), true);
+const postmanCollection = JSON.parse(postmanContent);
+
+describe('Har2Postman', () => {
+    it('Generates 0.6.0 output json (postman) given the 0.6.0 input json (har)', () => {
+        expect(expectedPostmanCollection).toEqual(postmanCollection);
+    });
+});
